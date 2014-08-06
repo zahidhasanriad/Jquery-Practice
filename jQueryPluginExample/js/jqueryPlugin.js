ed94e43bfccc58ab3c29920ecdd4ee3a46537f42
zahidhasanriad <zahid.riad@gmail.com>
2014-08-06 12:50:53 +0600

    jquery plugin style customisation

--- jQueryPluginExample/js/jqueryPlugin.js
+++ jQueryPluginExample/js/jqueryPlugin.js
@@ -1,10 +1,27 @@
 (function($) {
 
-    $.fn.helloWorld = function() {
+    $.fn.helloWorld = function( options ) {
 
-        return this.each( function() {
-            $(this).text("Hello FTFL!");
+        // Establish our default settings
+        var settings = $.extend({
+            text         : 'Hello, World!',
+            color        : null,
+            fontStyle    : null
+        }, options);
+
+        return this.each( function() 
+        {
+            $(this).text( settings.text );
+
+            if ( settings.color ) {
+            $(this).css( 'color', settings.color );
+            }
+
+            if ( settings.fontStyle ) {
+            $(this).css( 'font-style', settings.fontStyle );
+            }
         });
+
 
     }
 
